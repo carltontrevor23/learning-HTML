682e4567be7aad6ec18d45f94933acdbe5c05a88
Akanga  Andrew <sseluyindaeva@gmail.com>
2024-04-05 20:08:14 +0300

    l

---
+++ Assignment2.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+
+int main() {
+    double a = 3;
+    double b = 5;
+    double h = 4;
+
+    double area = (a + b) * h / 2;
+    std::cout << "The area of the trapezium is " << area << "\n\n\n";
+    return 0;
+}
