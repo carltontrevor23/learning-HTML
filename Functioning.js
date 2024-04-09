4ddd33988ad0cfe0c486d3544b7108489137c09d
Akanga Andrew <sseluyindaeva@gmail.com>
2024-04-09 14:21:17 +0300

    i am linking the webpages and also adding in comments to explain what i am doing

--- Functioning.js
+++ Functioning.js
@@ -1,14 +1,30 @@
 let index=0;
+//this is the counter of the images in the array
 let images=["Images/image7.jpeg","Images/familybreakfast.jpg","Images/family6.jpeg","Images/background.jpg"];
+//this is the array of the images to display
 function changeImage(){
     let imgElement=document.getElementById("slider")
+    //this is just variable so that i  not need to write that again
     if(imgElement){
         imgElement.src=images[index];
+        //this changes the source of the image to the image in the array at index when the function is called
         index=(index+1)%images.length;
+        //this increases the value of index but it's also check if the value of index exceeds the value of the length of the array
+        //this will reset it back to zero
     }
     else{
         console.error('Image not found');
+        //this will print in the console if the source of the image is not accessible
     }
     
 }
+//this will call the function changeImage every 3seconds
 setInterval(changeImage,3000);
+
+let popup = document.getElementById("popup");
+function openPopup(){
+    popup.classList.add("open");
+}
+function closePopup(){
+    popup.classList.remove("open"); 
+}
--- Functioning2.js
+++ Functioning2.js
@@ -1,14 +1,22 @@
 let index=0;
+//this is the counter of the images in the array
 let images=["Images/image6.jpg","Images/presidentialsuite.jpg","Images/banner3.jpg","Images/background.jpg"];
+//this is the array of the images to display
 function changeImage(){
     let imgElement=document.getElementById("slider")
+    //this is just variable so that i  not need to write that again
     if(imgElement){
         imgElement.src=images[index];
+        //this changes the source of the image to the image in the array at index when the function is called
         index=(index+1)%images.length;
+        //this increases the value of index but it's also check if the value of index exceeds the value of the length of the array
+        //this will reset it back to zero
     }
     else{
         console.error('Image not found');
+        //this will print in the console if the source of the image is not accessible
     }
     
 }
 setInterval(changeImage,3000);
+//this will call the function changeImage every 3seconds
